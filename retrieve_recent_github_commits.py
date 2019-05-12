b67371f1cd1a7107d124dc8e504ee6c600ed8cb3
ChristopherJamesN <nady.christopher@gmail.com>
2019-05-11 20:51:39 -0500

    Iterate over an array of input repos. Also added date to the values written to the text file.

--- retrieve_recent_github_commits.py
+++ retrieve_recent_github_commits.py
@@ -3,13 +3,15 @@ import json
 import formatter
 
 
-def retrieve_recent_github_commits(repo='dfs-insights-react-client'):
-    url = f'https://api.github.com/repos/christopherjamesn/{repo}/commits'
-    response = requests.get(url)
-    for value in response.json():
-        print(value['commit']['message'])
-        with open('todays_git_commits.txt', 'a') as open_file:
-            open_file.write(value['commit']['message'] + '\n')
+def retrieve_recent_github_commits(repos=['dfs-insights-react-client']):
+    for repo in repos:
+        url = f'https://api.github.com/repos/christopherjamesn/{repo}/commits'
+        response = requests.get(url)
+        for value in response.json():
+            print(value['commit']['message'])
+            with open('todays_git_commits.txt', 'a') as open_file:
+                open_file.write(value['commit']['committer']['date'] + '\n')
+                open_file.write(value['commit']['message'] + '\n')
 
 
 retrieve_recent_github_commits()
