824c99ce994c76763f33cd8c7d9309f19cc6ba68
ChristopherJamesN <nady.christopher@gmail.com>
2019-05-11 22:11:29 -0500

    Add authentication using a personal access token.

--- retrieve_recent_github_commits.py
+++ retrieve_recent_github_commits.py
@@ -1,12 +1,17 @@
 import requests
 import json
-import formatter
+import os
+from dotenv import load_dotenv
 
 
 def retrieve_recent_github_commits(repos=['dfs-insights-react-client']):
+    load_dotenv()
     for repo in repos:
+        with open('todays_git_commits.txt', 'a') as open_file:
+            open_file.write(repo + '\n')
         url = f'https://api.github.com/repos/christopherjamesn/{repo}/commits'
-        response = requests.get(url)
+        response = requests.get(
+            url, auth=('ChristopherJamesN', os.environ['PERSONAL_ACCESS_TOKEN']))
         for value in response.json():
             print(value['commit']['message'])
             with open('todays_git_commits.txt', 'a') as open_file:
