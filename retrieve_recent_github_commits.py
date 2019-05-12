29cf38dd74b9f9e9685b6d59c94236ad040a8539
ChristopherJamesN <nady.christopher@gmail.com>
2019-05-11 19:40:17 -0500

    Iterate over the json response and print the messages.

--- retrieve_recent_github_commits.py
+++ retrieve_recent_github_commits.py
@@ -1,10 +1,12 @@
 import requests
+import json
 
 
 def retrieve_recent_github_commits():
     url = 'https://api.github.com/repos/christopherjamesn/dfs-insights-react-client/commits'
     response = requests.get(url)
-    print(response.text)
+    for value in response.json():
+        print(value['commit']['message'])
 
 
 retrieve_recent_github_commits()
