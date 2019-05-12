77201f5974263340ab8e54e6ccbb5e08e2301187
ChristopherJamesN <nady.christopher@gmail.com>
2019-05-11 20:05:28 -0500

    Accept a repo, user dfs-insights-react-client as a default.

--- retrieve_recent_github_commits.py
+++ retrieve_recent_github_commits.py
@@ -1,9 +1,10 @@
 import requests
 import json
+import formatter
 
 
-def retrieve_recent_github_commits():
-    url = 'https://api.github.com/repos/christopherjamesn/dfs-insights-react-client/commits'
+def retrieve_recent_github_commits(repo='dfs-insights-react-client'):
+    url = f'https://api.github.com/repos/christopherjamesn/{repo}/commits'
     response = requests.get(url)
     for value in response.json():
         print(value['commit']['message'])
