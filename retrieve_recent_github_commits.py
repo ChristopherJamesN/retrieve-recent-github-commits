4be534dabb19190312555f46bb380a282e868542
Christopher Nady <christopher.nady@onit.com>
2019-06-10 16:41:33 -0500

    Update styling of commits retrieved to exclude data and time.

--- retrieve_recent_github_commits.py
+++ retrieve_recent_github_commits.py
@@ -12,16 +12,16 @@ def retrieve_recent_github_commits(repos=['test-word-react-add-in', 'word-add-in
     for repo in repos:
         with open('todays_git_commits.txt', 'a') as open_file:
             open_file.write('\n' + repo + '\n')
-        url = f'https://api.github.com/repos/christopherjamesn/{repo}/commits?page=1&per_page=10'
+        url = f'https://api.github.com/repos/christopherjamesn/{repo}/commits?page=1&per_page=25'
         response = requests.get(
             url, auth=('ChristopherJamesN', os.environ['PERSONAL_ACCESS_TOKEN']))
         for value in response.json():
             print(value['commit']['message'])
             with open('todays_git_commits.txt', 'a') as open_file:
                 day_string = value['commit']['committer']['date']
-                open_file.write('\n')
-                open_file.write(day_string + '\n')
-                open_file.write(value['commit']['message'] + '\n')
+                # open_file.write('\n')
+                #open_file.write(day_string + '\n')
+                open_file.write('*' + value['commit']['message'] + '\n')
 
 
 def retrieve_recent_gitlab_commits(repos=['test-word-react-add-in', 'word-add-in-pass-through-service', 'onit_java_sdk']):
